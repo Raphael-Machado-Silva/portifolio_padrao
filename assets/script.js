e68d4c790377bdac05430421637232f96783eecc
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2024-06-27 11:12:35 -0300

    Update javascript

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,33 @@
-// mostrar os outros projetos ao clicar no botão
-// sumir com o botão
-
 const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
 const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
 
-
 botaoMostrarProjetos.addEventListener('click', () => {
-    mostrarMaisProjetos();
-
-    esconderBotao();
+    if (botaoMostrarProjetos.classList.contains('mostrando')) {
+        mostrarMenosProjetos();
+    } else {
+        mostrarMaisProjetos();
+    }
+    alternarBotao();
 });
-
-function esconderBotao() {
-    botaoMostrarProjetos.classList.add('remover');
-}
 
 function mostrarMaisProjetos() {
     projetosInativos.forEach(projetosInativo => {
         projetosInativo.classList.add('ativo');
     });
+}
+
+function mostrarMenosProjetos() {
+    projetosInativos.forEach(projetosInativo => {
+        projetosInativo.classList.remove('ativo');
+    });
+}
+
+function alternarBotao() {
+    if (botaoMostrarProjetos.classList.contains('mostrando')) {
+        botaoMostrarProjetos.classList.remove('mostrando');
+        botaoMostrarProjetos.textContent = 'Mostrar Mais';
+    } else {
+        botaoMostrarProjetos.classList.add('mostrando');
+        botaoMostrarProjetos.textContent = 'Mostrar Menos';
+    }
 }
